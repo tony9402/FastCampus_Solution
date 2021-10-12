d40239d5b52f9a5619598d087d6f44b55c78774a
tony9402 <tony9402@naver.com>
2021-10-12 17:27:03 +0900

    [ADD] add SET_8 problem5 solution

---
+++ SET_8/04/main.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+vector<tuple<ll, ll, ll, int>> circle;
+vector<vector<int>> tree;
+
+pair<int, int> BFS(int S) {
+    pair<int, int> ret;
+    queue<pair<int, int>> Q;
+    vector<int> used(tree.size());
+    Q.emplace(S, 0);
+    used[S] = 1;
+    while(!Q.empty()) {
+        auto [cur, d] = Q.front(); Q.pop();
+        if(ret.first < d) {
+            ret.first = d;
+            ret.second = cur;
+        }
+        for(int nxt : tree[cur]) {
+            if(used[nxt]) continue;
+            used[nxt] = true;
+            Q.emplace(nxt, d + 1);
+        }
+    }
+    return ret;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N; cin >> N;
+    tree.resize(N + 1);
+    for(int i = 0; i < N; i++) {
+        int x, y, r; cin >> x >> y >> r;
+        circle.emplace_back(r, x, y, i + 1);
+    }
+    sort(circle.begin(), circle.end());
+    circle.emplace_back(1000000000, 0, 0, 0);
+    for(int i = 0; i <= N; i++) {
+        for(int j = i + 1; j <= N; j++) {
+            auto [ar, ax, ay, aidx] = circle[i];
+            auto [br, bx, by, bidx] = circle[j];
+            ll mr = ar - br;
+            ll d  = (ax - bx) * (ax - bx) + (ay - by) * (ay - by);
+            if((d == 0 && mr != 0) || d < mr * mr) {
+                tree[aidx].push_back(bidx);
+                tree[bidx].push_back(aidx);
+                break;
+            }
+        }
+    }
+
+    auto cur = BFS(0);
+    auto cur2 = BFS(cur.second);
+    cout << cur2.first;
+
+    return 0;
+}
