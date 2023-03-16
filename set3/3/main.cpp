510ef5c509c5fad9e153f6f242ff3085ce868fd4
tony9402 <tony9402@naver.com>
2023-03-17 02:07:07 +0900

    [MOD] 3-3 solutions

--- set3/3/main.cpp
+++ set3/3/main.cpp
@@ -9,8 +9,8 @@ int main() {
 
     int N, K; cin >> N >> K;
     vector<int> idx(N + 1), arr(N + 1), tmp(N + 1);
-    for(int i = 1; i <= N; i++)cin >> arr[i];
-    for(int i = 1; i <= N; i++)cin >> idx[i];
+    for(int i = 1; i <= N; i++) cin >> arr[i];
+    for(int i = 1; i <= N; i++) cin >> idx[i];
     for(int j = 1; j <= K; j++) {
         for(int i = 1; i <= N; i++) tmp[idx[i]] = arr[i];
         for(int i = 1; i <= N; i++) arr[i] = tmp[i];
